5c73f516dfa67d138d818a17bb0b3c479149128e
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-09-05 01:32:40 +0600

    Support out-of-tree builds
    Fixes #4

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Visual Studio Code project for Linux kernel sources
----------------------------------------------------
+===================================================
 
-Ensure the kernel is built (at least, all *.cmd files should be generated):
+Ensure the kernel is built (at least, all `*.cmd` files should be generated):
 
+    $ make defconfig
     $ make
 
 Clone this repository as ".vscode":
@@ -16,3 +17,20 @@ Generate compile_commands.json:
 Open the project:
 
     $ code .
+
+Out-of-tree builds
+------------------
+
+https://github.com/amezin/vscode-linux-kernel/issues/4
+
+Kernel can be built with separate output directory:
+
+    $ make O=../linux-build defconfig
+    $ make O=../linux-build
+
+In this case, you should pass the directory to `generate_compdb.py`:
+
+    $ python .vscode/generate_compdb.py -O ../linux-build
+
+`compile_commands.json` will still be generated in the current directory (root of the `linux` repository).
+Unfortunately, `tasks.json` will not work out of the box in this configuration (TODO).
--- generate_compdb.py
+++ generate_compdb.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division
 
+import argparse
 import fnmatch
+import functools
 import json
 import math
 import multiprocessing
@@ -12,15 +14,13 @@ import sys
 CMD_VAR_RE = re.compile(r'^\s*cmd_(\S+)\s*:=\s*(.+)\s*$', re.MULTILINE)
 SOURCE_VAR_RE = re.compile(r'^\s*source_(\S+)\s*:=\s*(.+)\s*$', re.MULTILINE)
 
-directory = os.path.abspath(os.getcwd())
-
 
 def print_progress_bar(progress):
     progress_bar = '[' + '|' * int(50 * progress) + '-' * int(50 * (1.0 - progress)) + ']'
     print('\r', progress_bar, "{0:.1%}".format(progress), end='\r', file=sys.stderr)
 
 
-def parse_cmd_file(cmdfile_path):
+def parse_cmd_file(out_dir, cmdfile_path):
     with open(cmdfile_path, 'r') as cmdfile:
         cmdfile_content = cmdfile.read()
 
@@ -28,18 +28,20 @@ def parse_cmd_file(cmdfile_path):
     sources = { match.group(1): match.group(2) for match in SOURCE_VAR_RE.finditer(cmdfile_content) }
 
     return [{
-            'directory': directory,
+            'directory': out_dir,
             'command': commands[o_file_name],
             'file': source,
             'output': o_file_name
         } for o_file_name, source in sources.items()]
 
 
-def main():
+def gen_compile_commands(out_dir):
     print("Building *.o.cmd file list...", file=sys.stderr)
 
+    out_dir = os.path.abspath(out_dir)
+
     cmd_files = []
-    for cur_dir, subdir, files in os.walk(directory):
+    for cur_dir, subdir, files in os.walk(out_dir):
         cmd_files.extend(os.path.join(cur_dir, cmdfile_name) for cmdfile_name in fnmatch.filter(files, '*.o.cmd'))
 
     print("Parsing *.o.cmd files...", file=sys.stderr)
@@ -50,7 +52,7 @@ def main():
     compdb = []
     pool = multiprocessing.Pool()
     try:
-        for compdb_chunk in pool.imap_unordered(parse_cmd_file, cmd_files, chunksize=int(math.sqrt(len(cmd_files)))):
+        for compdb_chunk in pool.imap_unordered(functools.partial(parse_cmd_file, out_dir), cmd_files, chunksize=int(math.sqrt(len(cmd_files)))):
             compdb.extend(compdb_chunk)
             n_processed += 1
             print_progress_bar(n_processed / len(cmd_files))
@@ -61,8 +63,15 @@ def main():
 
     print(file=sys.stderr)
     print("Writing compile_commands.json...", file=sys.stderr)
+
     with open('compile_commands.json', 'w') as compdb_file:
         json.dump(compdb, compdb_file, indent=1)
+
+
+def main():
+    cmd_parser = argparse.ArgumentParser()
+    cmd_parser.add_argument('-O', '--out-dir', type=str, default=os.getcwd(), help="Build output directory")
+    gen_compile_commands(**vars(cmd_parser.parse_args()))
 
 
 if __name__ == '__main__':
